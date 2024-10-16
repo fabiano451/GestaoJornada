a5b3e5f96a9710046d74cd26a528cf807ba2a550
fabiano451 <58607659+fabiano451@users.noreply.github.com>
2024-10-16 10:02:07 -0300

    Update AlunoConfiguration.cs

--- Jornada.Repository/Mapping/AlunoConfiguration.cs
+++ Jornada.Repository/Mapping/AlunoConfiguration.cs
@@ -13,13 +13,13 @@ namespace Jornada.Repository.Mapping
     {
         public void Configure(EntityTypeBuilder<Aluno> builder)
         {
-            builder.ToTable("Aluno");
+            builder.ToTable("Alunos");
             builder.HasKey(a => a.AlunoId).HasName("Pk_Aluno");
-            builder.Property(a => a.AlunoId).HasColumnName("AlunoId").HasColumnType("UNIQUEIDENTIFIER").IsRequired();
+            builder.Property(a => a.AlunoId).HasColumnName("AlunoId").HasColumnType("int").IsRequired();
             builder.Property(a => a.Nome).HasColumnName("Nome").HasColumnType("nvarchar(100").IsRequired();
-            builder.Property(a => a.CPF).HasColumnName("CPF").HasColumnType("UNIQUEIDENTIFIER").IsRequired();
+            builder.Property(a => a.CPF).HasColumnName("CPF").HasColumnType("varchar(11)").IsRequired();
             builder.Property(a => a.Email).HasColumnName("Email").HasColumnType("nvarchar(100").IsRequired();
-            builder.Property(a => a.Telefone).HasColumnName("Telefone").HasColumnType("nvarchar(100").IsRequired();
+            builder.Property(a => a.Telefone).HasColumnName("Telefone").HasColumnType("nvarchar(15").IsRequired();
             builder.Property(a => a.Endereco).HasColumnName("Endereco").HasColumnType("nvarchar(200").IsRequired();
             builder.Property(a => a.DataCadastro).HasColumnName("DataCadastro").HasColumnType("DATETIME").IsRequired();
         }
